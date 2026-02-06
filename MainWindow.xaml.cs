5a333a24ea3eddc408e3171c6587ff7b88b4b581
AdvancedPower <frc.program.815@outlook.com>
2026-02-05 23:01:54 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MotorController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Motor> Motors { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Motors = new ObservableCollection<Motor>
+            {
+                new Motor { ID = 1, Speed = 28 },
+                new Motor { ID = 2, Speed = -35 },
+                new Motor { ID = 3, Speed = 42 }
+            };
+
+            // Set DataContext for binding
+            DataContext = this;     
+        }
+    }
+}
---
+++ Motor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotorController
+{
+    public class Motor
+    {
+        public int ID { get; set; }
+        public int Speed { get; set; }
+    }
+}
